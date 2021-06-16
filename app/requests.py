609aa9600da3a4d74856c8aac8b90bacaef98393
Dennis Peters <dennis95peters@gmail.com>
2021-06-17 02:40:02 +0300

    Test transaction capabilities

--- app/requests.py
+++ app/requests.py
@@ -1,6 +1,6 @@
-
 import urllib.request,json
 import requests 
+
 
 # Getting api key
 api_key = None
@@ -10,6 +10,28 @@ def configure_request(app):
     global api_key
     api_key = app.config['API_KEY']
 
+def get_currencies():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_currencies_url = f"https://free.currconv.com/api/v7/currencies?apiKey={api_key}"
+    currency_data = requests.get(get_currencies_url).json()["results"]
+    
+    currencies = []
+    for data in currency_data:
+         currencies.append(data)
+   
+
+    return currencies
+    
+def get_currency_rates(initalCurrency,ConvertToCurrency):
+    '''
+    Function that gets the json response to our url request
+    '''
+    rates_url = f"https://free.currconv.com/api/v7/convert?q={initalCurrency}_{ConvertToCurrency}&compact=ultra&apiKey={api_key}"
+    rate = requests.get( rates_url).json()
+    
+    return rate
 
     
     
