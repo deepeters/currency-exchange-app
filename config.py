7df38f39beaa52e5673df227cdc69ddc291e420f
Dennis Peters <dennis95peters@gmail.com>
2021-06-19 01:11:49 +0300

    Fix minor bugs

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
 #     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = "postgresql://dulfrqrkamwwcb:109cdefcd744280f24480af769762f1158267b87ae59ace20d612555b2869f07@ec2-34-232-191-133.compute-1.amazonaws.com:5432/d21mkvhn8nmuc9?sslmode=require"
 
 
 class DevConfig(Config):
